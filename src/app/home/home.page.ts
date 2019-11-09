ec9dc80e9930c8d8b4b2bb95884f7dea9305cf2b
Francisco Martinez <isa.franciscomtz@hotmail.com>
2019-11-09 08:05:28 -0600

    Actualizacion de archivos.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private flashlight: Flashlight) { }
+
+  switchFlash( evento ) { /*Recivimos en evento (ion-Change) del componente ion-toggle*/
+
+    const power: boolean = evento.target.checked; /*Declaramos una variable con el valor del estado de neustro toggle true/false*/
+    if (power) { /*Condicion para activar y desactivar flash*/
+      console.log('On');   /*Impresion en consola para comporbar estado*/
+      this.flashlight.switchOn(); /*Accedemos a la funcion de encender flsh de nuestro objeto flashlight */
+    } else {
+      console.log('Off'); /*Impresion en consola para comporbar estado*/
+      this.flashlight.switchOff(); /*Accedemos a la funcion de apagar flsh de nuestro objeto flashlight */
+    }
+  }
 
 }
